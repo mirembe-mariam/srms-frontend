c80888ff0edab99eb5f8e4d605e96949cc15bd7a
mirembe-mariam <mirembemariam3@gmail.com>
2023-10-04 12:06:36 +0300

    created login component and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/*" element={<Navigate to="/login" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./Login.css";
+
+import { useNavigate } from "react-router-dom";
+const Login = () => {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  
+  const handleLogin = () => {
+    if (username === "rhodin" && password === "1234") {
+      setIsLoggedIn(true);
+      navigate("/dashboard");
+    } else {
+      setError("Invalid credentials ! Please try again.");
+    }
+  };
+  return (
+    <div className="login-page">
+      <h2>Student Results Management System</h2>
+      <div className="login-card">
+        <h3>Login to access your account </h3>
+        <label for="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+        <br />
+        <label for="password">Password:</label>
+        <input
+          type="text"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <br />
+        <button onClick={handleLogin}>Login</button>
+      </div>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
+};
+
+export default Login;
