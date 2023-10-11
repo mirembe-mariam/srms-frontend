a98d8058baf4527374eda424b2f3056b73f934d4
mirembe-mariam <mirembemariam3@gmail.com>
2023-10-11 12:26:20 +0300

    added the button and table for the class component

--- src/Components/Classes/Classcontent.tsx
+++ src/Components/Classes/Classcontent.tsx
@@ -1,13 +1,70 @@
 import React from "react";
 import "./classes.css";
+
 const Classcontent = () => {
   return (
     <div className="content">
-      <h3 className="header">Class Listing</h3>
-      {/* <hr /> */}
-      <div>
-       
+      <div className="header-items">
+        <h3 className="header">Class Listing</h3>
+        <input type="text" placeholder="Search" className="search-input" />
       </div>
+      <div className="actions">
+        <button className="add-button"> + Add Class</button>
+      </div>
+      <table className="class-table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Level</th>
+            <th>Section</th>
+            <th>Status</th>
+            <th>Date/Time</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Row 1, Cell 1</td>
+            <td>Row 1, Cell 2</td>
+            <td>Row 1, Cell 3</td>
+            <td>Row 1, Cell 4</td>
+            <td>Row 1, Cell 5</td>
+            <td>Row 1, Cell 6</td>
+          </tr>
+          <tr>
+            <td>Row 2, Cell 1</td>
+            <td>Row 2, Cell 2</td>
+            <td>Row 2, Cell 3</td>
+            <td>Row 2, Cell 4</td>
+            <td>Row 2, Cell 5</td>
+            <td>Row 2, Cell 6</td>
+          </tr>
+          <tr>
+            <td>Row 2, Cell 1</td>
+            <td>Row 2, Cell 2</td>
+            <td>Row 2, Cell 3</td>
+            <td>Row 2, Cell 4</td>
+            <td>Row 2, Cell 5</td>
+            <td>Row 2, Cell 6</td>
+          </tr>
+          <tr>
+            <td>Row 2, Cell 1</td>
+            <td>Row 2, Cell 2</td>
+            <td>Row 2, Cell 3</td>
+            <td>Row 2, Cell 4</td>
+            <td>Row 2, Cell 5</td>
+            <td>Row 2, Cell 6</td>
+          </tr>
+          <tr>
+            <td>Row 2, Cell 1</td>
+            <td>Row 2, Cell 2</td>
+            <td>Row 2, Cell 3</td>
+            <td>Row 2, Cell 4</td>
+            <td>Row 2, Cell 5</td>
+            <td>Row 2, Cell 6</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
